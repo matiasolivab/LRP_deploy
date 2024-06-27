34a1fc8533916a8ae80724fce3608c6787b8bcb7
Matias Oliva <matiasolivab@uc.cl>
2024-06-27 02:28:15 -0400

    ok

--- src/Hooks/useCellAction.js
+++ src/Hooks/useCellAction.js
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import axios from 'axios';
 
-const cellAction = async ({gameId, currentPlayer, token}) => {
+const cellAction = async ({ gameId, currentPlayer, token }) => {
     try {
         const response = await axios.post(
             `${import.meta.env.VITE_BACK_URL}/game/cellAction/${gameId}`,
-            { playerId: currentPlayer.playerId},
+            { playerId: currentPlayer.playerId },
             {
                 headers: {
                     'Content-Type': 'application/json',
@@ -17,29 +17,32 @@ const cellAction = async ({gameId, currentPlayer, token}) => {
     } catch (error) {
         console.error(error);
     }
+};
 
-}
+export default function useCellAction({ gameId, token, currentPlayer, socket, setNotificationMessage, setIsNotificationVisible, setCanThrowDice }) {
+    const isInitialMount = useRef(true);
 
-export default function useCellAction({gameId, token, currentPlayer, socket, setNotificationMessage, setIsNotificationVisible, setCanThrowDice}) {
-    useEffect(()=> {
-        if (currentPlayer && currentPlayer.boardPosition !== undefined && !([5,15,20,25,35].includes(currentPlayer.boardPosition)) ) {
-            cellAction({gameId, currentPlayer, token}).then((data) => {
-                const regex = /^Los gemelos pueden lanzar los dados otra vez. (.+)$/;
-                if (regex.test(data?.result?.message)) {
-                    setCanThrowDice(true);
-                   }
-            
-                if (data?.result?.message) {
-                    setNotificationMessage(data.result.message);
-                    setIsNotificationVisible(true);
-                    setTimeout(() => {
-                        setIsNotificationVisible(false);
-                    }, 5000);
-                }
-                socket.emit('gameAction', gameId);
-            });
+    useEffect(() => {
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+        } else {
+            if (currentPlayer && currentPlayer.boardPosition !== undefined && !([5, 15, 20, 25, 35].includes(currentPlayer.boardPosition))) {
+                cellAction({ gameId, currentPlayer, token }).then((data) => {
+                    const regex = /^Los gemelos pueden lanzar los dados otra vez. (.+)$/;
+                    if (regex.test(data?.result?.message)) {
+                        setCanThrowDice(true);
+                    }
 
-            
+                    if (data?.result?.message) {
+                        setNotificationMessage(data.result.message);
+                        setIsNotificationVisible(true);
+                        setTimeout(() => {
+                            setIsNotificationVisible(false);
+                        }, 5000);
+                    }
+                    socket.emit('gameAction', gameId);
+                });
+            }
         }
-    },[currentPlayer?.boardPosition])
+    }, [currentPlayer?.boardPosition]);
 }
